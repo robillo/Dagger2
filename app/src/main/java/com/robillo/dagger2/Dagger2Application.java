c722581488931d19f2b3b011f91099eed20027a5
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-09-14 19:33:46 +0530

    completed example of Dagger 2 implementation

--- app/src/main/java/com/robillo/dagger2/Dagger2Application.java
+++ app/src/main/java/com/robillo/dagger2/Dagger2Application.java
@@ -2,9 +2,33 @@ package com.robillo.dagger2;
 
 import android.app.Application;
 
+import com.robillo.dagger2.dagger.AppComponent;
+import com.robillo.dagger2.dagger.AppModule;
+import com.robillo.dagger2.dagger.DaggerAppComponent;
+
 /**
  * Created by robinkamboj on 14/09/17.
  */
 
-public class Dagger2Application extends Application{
+public class Dagger2Application extends Application {
+
+    private static Dagger2Application instance = new Dagger2Application();
+    private static AppComponent appComponent;
+
+    public static Dagger2Application getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        getAppComponent();
+    }
+
+    public AppComponent getAppComponent() {
+        if(appComponent == null){
+            appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).build();
+        }
+        return appComponent;
+    }
 }
--- app/src/main/java/com/robillo/dagger2/ProductPresenter.java
+++ app/src/main/java/com/robillo/dagger2/ProductPresenter.java
@@ -1,8 +1,44 @@
 package com.robillo.dagger2;
 
+import com.robillo.dagger2.model.LineItem;
+import com.robillo.dagger2.model.Product;
+
+import javax.inject.Inject;
+
 /**
  * Created by robinkamboj on 14/09/17.
  */
 
 public class ProductPresenter {
+
+    //We are creating a class member variable for the
+    //Shopping cart that we will be injecting to this class
+    @SuppressWarnings("WeakerAccess")
+    @Inject
+    ShoppingCart mCart;
+
+    public ProductPresenter(){
+        //Here is where the actual injection takes place
+        Dagger2Application.getInstance().getAppComponent().inject(this);
+    }
+
+    //Here is an example of how we are using the injected shopping cart
+    public void onItemQuantityChanged(LineItem item, int qty) {
+        mCart.updateItemQty(item, qty);
+    }
+
+    //Another example of using the shopping cart
+    public void onAddToCartButtonClicked(Product product) {
+        //perform add to checkout button here
+        LineItem item = new LineItem(product, 1);
+        mCart.addItemToCart(item);
+    }
+
+    public void onClearButtonClicked() {
+        mCart.clearShoppingCart();
+    }
+
+    public void onDeleteItemButtonClicked(LineItem item) {
+        mCart.removeItemFromCart(item);
+    }
 }
